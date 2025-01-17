45150d6558ca6e51bc35ca678ed615f9ee1b84f2
LucasCherbuin <lucas.cherbuin@gmail.com>
2025-01-17 22:47:56 +0100

    ajout des menus utilisateursadmin, employee et veterinaires

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MenuController extends AbstractController
+{
+    #[Route('/admin', name: 'admin_menu')]
+    public function admin(): Response
+    {
+        // Vérification des rôles de l'utilisateur connecté
+        $user = $this->getUser();
+        dump($user); // Affiche l'utilisateur
+        dump($user ? $user->getRoles() : 'Utilisateur non connecté'); // Affiche les rôles de l'utilisateur
+
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException('Accès interdit');
+        }
+
+        return $this->render('admin/admin.html.twig', [
+            'controller_name' => 'MenuController',
+        ]);
+    }
+
+    #[Route('/employee', name: 'employee_menu')]
+    public function employee(): Response
+    {
+        // Vérification de l'utilisateur et des rôles
+        $user = $this->getUser();
+        dump($user);
+        dump($user ? $user->getRoles() : 'Utilisateur non connecté');
+
+        if (!$this->isGranted('ROLE_EMPLOYEE')) {
+            throw $this->createAccessDeniedException('Accès interdit');
+        }
+
+        return $this->render('employee/employee.html.twig', [
+            'controller_name' => 'MenuController',
+        ]);
+    }
+
+    #[Route('/veterinaire', name: 'veterinaire_menu')]
+    public function veterinaire(): Response
+    {
+        // Vérification de l'utilisateur et des rôles
+        $user = $this->getUser();
+        dump($user);
+        dump($user ? $user->getRoles() : 'Utilisateur non connecté');
+
+        if (!$this->isGranted('ROLE_VETERINAIRE')) {
+            throw $this->createAccessDeniedException('Accès interdit');
+        }
+
+        return $this->render('veterinaire/veterinaire.html.twig', [
+            'controller_name' => 'MenuController',
+        ]);
+    }
+}
+
+ 
