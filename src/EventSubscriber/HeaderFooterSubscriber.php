b33f2579ccc64d42ea385fb2b4bb4521e385f8c0
LucasCherbuin <lucas.cherbuin@gmail.com>
2025-01-18 22:36:32 +0100

    ajout de différents subscriber pour le fonctionnement des entités

---
+++ src/EventSubscriber/HeaderFooterSubscriber.php
@@ -0,0 +1,59 @@
+<?php
+
+// src/EventSubscriber/HeaderFooterSubscriber.php
+namespace App\EventSubscriber;
+
+use App\Controller\HeaderFooterController;
+use App\Repository\HoraireRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class HeaderFooterSubscriber implements EventSubscriberInterface
+{
+    private HoraireRepository $horaireRepository;
+
+    public function __construct(HoraireRepository $horaireRepository)
+    {
+        $this->horaireRepository = $horaireRepository;
+    }
+
+    public function onKernelController(ControllerEvent $event)
+    {
+        $controller = $event->getController();
+
+        // Vérifie si le contrôleur actuel est une instance de HeaderFooterController
+        if (is_array($controller) && $controller[0] instanceof HeaderFooterController) {
+            $controllerObject = $controller[0];
+
+            // Récupère les horaires depuis le repository et prépare-les
+            $horaires = $this->prepareHoraires($this->horaireRepository->findAll());
+
+            // Modifie les horaires dans le contrôleur
+            $controllerObject->setGlobalHoraires($horaires);
+        }
+    }
+
+    private function prepareHoraires(array $horaires): array
+    {
+        // Préparer les horaires pour un affichage propre
+        $preparedHoraires = [];
+        foreach ($horaires as $horaire) {
+            $preparedHoraires[] = [
+                'jour' => $horaire->getJour(),
+                'ouverture' => $horaire->getOuverture()->format('H:i'),
+                'fermeture' => $horaire->getFermeture()->format('H:i'),
+            ];
+        }
+
+        return $preparedHoraires;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => 'onKernelController',
+        ];
+    }
+}
+
