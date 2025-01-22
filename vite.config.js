978c1c01c8ea9ff9f0adca1bbb20b4812b9529cf
LucasCherbuin <lucas.cherbuin@gmail.com>
2025-01-22 16:35:40 +0100

    correction

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: 'public',
-    build: {
-      outDir: '../public/', // Dossier de sortie
-      rollupOptions: {
-        input: 'index.html', // fichier d'entrée
+  root: 'public', // Définit 'public' comme dossier racine
+  build: {
+    outDir: '../dist', // Place les fichiers générés dans le dossier "dist" à la racine
+    emptyOutDir: true, // Nettoie le dossier de sortie avant chaque build
+    rollupOptions: {
+      input: 'public/index.html', // Chemin relatif au fichier d'entrée HTML
     },
   },
 });
