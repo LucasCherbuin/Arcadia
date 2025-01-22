50c400f3ec7ca83641d0b4e9a6a4656f0ba14988
LucasCherbuin <lucas.cherbuin@gmail.com>
2025-01-22 16:50:25 +0100

    Fix Vite build for PHP project

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'public', // Spécifie que la racine du projet est le dossier public
   build: {
-    outDir: '../public', // Dossier de sortie des fichiers
+    outDir: '../public', // Dossier de sortie des fichiers générés
     rollupOptions: {
-      input: 'index.php', // Fichier d'entrée
+      input: 'public/index.html', // Utiliser index.html comme point d'entrée
     },
   },
 });
