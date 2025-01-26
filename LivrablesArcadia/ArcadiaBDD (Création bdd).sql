e7ae548d74a0bea70fbd35f88e39245ca0727eac
LucasCherbuin <lucas.cherbuin@gmail.com>
2025-01-26 22:53:27 +0100

    ajout des livrables coté documents ainsi que la mise a jour du .env de production

---
+++ LivrablesArcadia/ArcadiaBDD (Création bdd).sql
@@ -0,0 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : dz8959rne9lumkkw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com
+-- Généré le : dim. 26 jan. 2025 à 22:36
+-- Version du serveur : 8.0.35
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : xlrq5uzk8kkz5lgg
+--
+CREATE DATABASE IF NOT EXISTS xlrq5uzk8kkz5lgg DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE xlrq5uzk8kkz5lgg;
