5f1c6c02a26d87c05e612755ebed5d6871467d18
LucasCherbuin <lucas.cherbuin@gmail.com>
2025-01-29 22:23:41 +0100

    suppression de sncRedis

--- config/bundles.php
+++ config/bundles.php
@@ -13,8 +13,4 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
